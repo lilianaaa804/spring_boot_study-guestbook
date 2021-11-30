ddc9d566c31fa747fb4496d551a92cff9d033a21
lilianaaa804 <lilianaaa804@gmail.com>
2021-12-01 03:20:44 +0900

    목록출력, 페이징(링크 제외)

--- src/main/java/com/liliana/guestbook/controller/GuestbookController.java
+++ src/main/java/com/liliana/guestbook/controller/GuestbookController.java
@@ -1,19 +1,33 @@
 package com.liliana.guestbook.controller;
 
+import com.liliana.guestbook.dto.PageRequestDTO;
+import com.liliana.guestbook.dto.PageResultDTO;
+import com.liliana.guestbook.service.GuestbookService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/guestbook")
 @Log4j2
+@RequiredArgsConstructor
 public class GuestbookController {
 
-    @GetMapping({"/","/list"})
-    public String list() {
-        log.info("list................");
+    private final GuestbookService service;
 
-        return "/guestbook/list";
+    @GetMapping("/")
+    public String list() {
+
+        return "redirect:/guestbook/list";
     }
+
+    @GetMapping("/list")
+    public void list(PageRequestDTO pageRequestDTO, Model model){
+        log.info("list................" + pageRequestDTO);
+        model.addAttribute("result", service.getList(pageRequestDTO));
+    }
+
 }
