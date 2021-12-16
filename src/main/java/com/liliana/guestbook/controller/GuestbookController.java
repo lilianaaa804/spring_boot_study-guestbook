87e6fbb408c1080d9152df59798bbb6b5d873101
lilianaaa804 <lilianaaa804@gmail.com>
2021-12-17 02:08:41 +0900

    등록페이지, 등록, 조회 링크 (~p.186)

--- src/main/java/com/liliana/guestbook/controller/GuestbookController.java
+++ src/main/java/com/liliana/guestbook/controller/GuestbookController.java
@@ -1,5 +1,6 @@
 package com.liliana.guestbook.controller;
 
+import com.liliana.guestbook.dto.GuestbookDTO;
 import com.liliana.guestbook.dto.PageRequestDTO;
 import com.liliana.guestbook.dto.PageResultDTO;
 import com.liliana.guestbook.service.GuestbookService;
@@ -8,7 +9,9 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/guestbook")
@@ -28,6 +31,22 @@ public class GuestbookController {
     public void list(PageRequestDTO pageRequestDTO, Model model){
         log.info("list................" + pageRequestDTO);
         model.addAttribute("result", service.getList(pageRequestDTO));
+    }
+
+    @GetMapping("/register")
+    public void register(){
+        log.info("register get........");
+    }
+
+    @PostMapping("/register")
+    public String registerPost(GuestbookDTO dto, RedirectAttributes redirectAttributes){
+        log.info("dto......" + dto);
+
+        Long gno = service.register(dto);
+
+        redirectAttributes.addFlashAttribute("msg", gno);
+
+        return "redirect:/guestbook/list";
     }
 
 }
